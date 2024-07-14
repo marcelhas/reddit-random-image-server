20b2441879633bcc28d5316d4b25c306e2cabe47
marcelhas <marcel.hasieber@gmail.com>
2024-07-14 14:47:09 +0200

    Use custom user agent to avoid 429 errors

--- src/reddit.js
+++ src/reddit.js
@@ -1,7 +1,10 @@
 const axios = require('axios');
 
 const fetchRandomTopPost = async (subreddit, by) => {
-  const res = await axios.get(`https://www.reddit.com/r/${subreddit}/top.json?t=${by}`);
+  const headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 GLS/100.10.9939.100"
+  }
+  const res = await axios.get(`https://www.reddit.com/r/${subreddit}/top.json?t=${by}`, { headers });
   if (res.length <= 0)
     return null
 
